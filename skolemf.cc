77713c3012e649b06bf1583853e50a91a207e645
philippeitis <33013301+philippeitis@users.noreply.github.com>
2018-03-31 17:16:24 -0700

    Maximum Optimization
    Compile using "g++ -O3 -o output skolemf.cc" and run with ./output (if using Linux based systems) for optimal performance

---
+++ skolemf.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <ctime>
+#include <algorithm>
+
+
+int main() {
+    const int ColWidth = 24;
+    const int ColWidthk = 6;
+
+    // prints things
+
+    std::cout << "k, # of sequences, computational time" << std::endl;
+
+    int permutation[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+
+    for(register unsigned int k = 0; k<14; ++k) {
+
+        int count = 0;
+        clock_t begin = clock();
+
+           if(k%4 != 2 && k%4 != 3){
+
+                do {
+                    register unsigned int trials = 0;
+                    register unsigned int pos = 0;
+                    bool skolem[2*k]= {false};
+
+                    for (register unsigned int x = 0; x < k; ++x) {
+
+                        if(skolem[pos+permutation[x]] || pos+permutation[x] >= 2*k) {
+                            break;
+                        }
+
+                        skolem[pos] = true; skolem[pos+permutation[x]] = true;
+                        
+                        while(skolem[pos]) {
+                            ++pos;
+                        }
+
+                        ++trials;
+
+                    }
+
+                    if(trials==k){++count;}
+
+            } while (std::next_permutation(permutation,permutation+k));
+        }
+
+        clock_t end = clock();
+        double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+
+        std::cout << k << ", " << count << ", " << elapsed_secs << std::endl;
+    }
+
+    return 0;    
+}
+
