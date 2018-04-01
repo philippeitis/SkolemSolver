21347f47b21478dd8a6108caa00690cda42b8db2
philippeitis <33013301+philippeitis@users.noreply.github.com>
2018-03-31 17:18:12 -0700

    Update skolemf.cc

--- skolemf.cc
+++ skolemf.cc
@@ -4,11 +4,6 @@
 
 
 int main() {
-    const int ColWidth = 24;
-    const int ColWidthk = 6;
-
-    // prints things
-
     std::cout << "k, # of sequences, computational time" << std::endl;
 
     int permutation[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
